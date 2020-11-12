4533f3ac45c09f9a87a7f94d745167b2791e664c
Shane Jeremich <shane.jeremich@gmail.com>
2020-11-12 00:48:34 -0700

    added more files, finished chapter 6 😁

---
+++ 06-Destructuring.js
@@ -0,0 +1,11 @@
+var obj = { first: "Gavin", last: "Kirlin" }
+var firstName = obj.first
+var lastName = obj.last
+
+console.log(obj)
+console.log(firstName)
+console.log(lastName)
+
+const { first, second } = { first: 0, second: 1 }
+console.log(first)
+console.log(second)
