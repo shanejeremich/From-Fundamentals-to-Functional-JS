0659790b5ae9e4643d15d4935ffe9a964ba6acae
Shane Jeremich <shane.jeremich@gmail.com>
2020-11-12 02:39:22 -0700

    added more examples of destructuring

--- 06-Destructuring.js
+++ 06-Destructuring.js
@@ -6,6 +6,21 @@ console.log(obj)
 console.log(firstName)
 console.log(lastName)
 
-const { first, second } = { first: 0, second: 1 }
+const { first, second } = { second: 10, first: 15 }
 console.log(first)
 console.log(second)
+
+const { name, weapon, room } = {
+	name: "Rusty",
+	room: "kitchen",
+	weapon: "candlestick",
+}
+
+console.log(name, weapon, room)
+
+const [a, , b] = [1, 2, 3]
+console.log(a, b)
+
+const [c, ...d] = [1, 2, 3]
+console.log("d", d)
+console.log(c, d)
